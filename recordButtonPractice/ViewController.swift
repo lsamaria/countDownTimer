f841f11e8c0e439c5dbf0ea63d18d7209fa21da2
lsamaria <lsamaria@aol.com>
2020-07-03 04:02:06 -0400

    v.2 -added square and fixed timer/basicAnimation start/then restart issue

--- recordButtonPractice/ViewController.swift
+++ recordButtonPractice/ViewController.swift
@@ -5,16 +5,19 @@
 //  Created by Lance Samaria on 6/27/20.
 //  Copyright © 2020 Lance Samaria. All rights reserved.
 //
+//  https://www.calayer.com/core-animation/2017/12/25/cashapelayer-in-depth-part-ii.html
 
 import UIKit
 
 class ViewController: UIViewController {
     
     
-    //MARK:- UIElements
+    //MARK:- Animations
+    fileprivate let shapeLayer = CAShapeLayer()
     fileprivate let bgShapeLayer = CAShapeLayer()
-    fileprivate let timerShapeLayer = CAShapeLayer()
-    fileprivate let basicAnimation = CABasicAnimation(keyPath: "strokeEnd") // transform.rotation.z
+    fileprivate var basicAnimation: CABasicAnimation!
+    
+    //MARK:- UIElements
     fileprivate lazy var cameraButton: UIButton = {
         let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
@@ -36,6 +39,13 @@ class ViewController: UIViewController {
         return label
     }()
     
+    fileprivate lazy var box: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .brown
+        return view
+    }()
+    
     //MARK:- Properties
     
     /*
@@ -52,15 +62,15 @@ class ViewController: UIViewController {
     
     
      // *** SECONDS ONLY ***
-     fileprivate var preRecordMaxVideoTimeInSecs = 15
-     fileprivate lazy var seconds = preRecordMaxVideoTimeInSecs
-     fileprivate var milliseconds = 0
-     fileprivate lazy var timerStr = initialStrForTimerLabel
-     fileprivate lazy var initialStrForTimerLabel = "\(preRecordMaxVideoTimeInSecs).0"
-     
+    fileprivate var preRecordMaxVideoTimeInSecs = 15
+    fileprivate lazy var seconds = preRecordMaxVideoTimeInSecs
+    fileprivate var milliseconds = 0
+    fileprivate lazy var timerStr = initialStrForTimerLabel
+    fileprivate lazy var initialStrForTimerLabel = "\(preRecordMaxVideoTimeInSecs).0" // "\(preRecordMaxVideoTimeInSecs)"
     
     fileprivate weak var videoTimer: Timer?
-    fileprivate var startStopTimer = true
+    
+    fileprivate var timerAnimation: CABasicAnimation?
     
     //MARK:- View Controller Lifecycle
     override func viewDidLoad() {
@@ -82,29 +92,14 @@ class ViewController: UIViewController {
             
             cameraButton.layer.cornerRadius = cameraButton.frame.width / 2
             
-            //addBothCAShapeLayersToCameraButton()
+            addBothCAShapeLayersToCameraButton()
         }
     }
     
     //MARK:- Target Actions
     @objc fileprivate func cameraButtonPressed() { }
     
-    //MARK:- Anchors
-    fileprivate func configureAnchors() {
-        
-        view.addSubview(cameraButton)
-        view.addSubview(timerLabel)
-        
-        cameraButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        cameraButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        cameraButton.widthAnchor.constraint(equalToConstant: 75).isActive = true
-        cameraButton.heightAnchor.constraint(equalToConstant: 75).isActive = true
-        
-        timerLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        timerLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
-        timerLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
-    }
-    
+    //MARK:- Animations Methods
     fileprivate func addBothCAShapeLayersToCameraButton() {
         
         let center = cameraButton.center
@@ -112,67 +107,140 @@ class ViewController: UIViewController {
         let lineWidth: CGFloat = 6
         let fillColor = UIColor.clear.cgColor
         
+        /* circle- */
         let bgCircularPath = UIBezierPath(arcCenter: center,
                                           radius: radius,
                                           startAngle: -.pi/2,
                                           endAngle: 3 * .pi/2,
                                           clockwise: true)
-        bgShapeLayer.path = bgCircularPath.cgPath
+        //bgShapeLayer.path = bgCircularPath.cgPath
+        /* circle- */
+        
+        /* box- */
+        bgShapeLayer.frame = box.bounds
+        bgShapeLayer.path = UIBezierPath(rect: box.bounds).cgPath
+        /* box- */
+        
         bgShapeLayer.strokeColor = UIColor.lightGray.cgColor
         bgShapeLayer.fillColor = fillColor
         bgShapeLayer.lineWidth = lineWidth
-        view.layer.addSublayer(bgShapeLayer)
-        view.layer.insertSublayer(bgShapeLayer, at: 0)
-        let timerCircularPath = UIBezierPath(arcCenter: center,
+        
+        /* circle- */
+        //view.layer.addSublayer(bgShapeLayer)
+        //view.layer.insertSublayer(bgShapeLayer, at: 0)
+        /* circle- */
+        
+        /* box- */
+        box.layer.addSublayer(bgShapeLayer)
+        box.layer.insertSublayer(bgShapeLayer, at: 0)
+        /* box- */
+        
+        /* circle- */
+        let circularPath = UIBezierPath(arcCenter: center,
                                              radius: radius,
                                              startAngle: -.pi/2,
                                              endAngle: 3 * .pi/2,
                                              clockwise: true)
-        timerShapeLayer.path = timerCircularPath.cgPath
-        timerShapeLayer.strokeColor = UIColor.red.cgColor
-        timerShapeLayer.fillColor = fillColor
-        timerShapeLayer.lineWidth = lineWidth
+        //shapeLayer.path = circularPath.cgPath
+        /* cicrle- */
         
-        timerShapeLayer.strokeEnd = 0
-        //camerButtonTimerLeftShapeLayer.speed = 0 // pause
+        /* box- */
+        shapeLayer.frame = box.bounds
+        shapeLayer.path = UIBezierPath(rect: box.bounds).cgPath
+        /* box- */
         
-        //view.layer.addSublayer(timerShapeLayer)
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.fillColor = fillColor
+        shapeLayer.lineWidth = lineWidth
+        shapeLayer.lineCap = .round
+        shapeLayer.strokeEnd = 0
+        
+        /* circle- */
+        //view.layer.addSublayer(shapeLayer)
+        /* circle- */
+        
+        /* box- */
+        box.layer.addSublayer(shapeLayer)
+        /* box- */
+        
+        //addProgressAnimation()
     }
     
     fileprivate var isBasicAnimationAnimating = false
     fileprivate func addProgressAnimation() {
         
-        removeProgressAnimation()
+        CATransaction.begin()
+        
+        basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        
+        removeAnimation()
+        
+        if shapeLayer.timeOffset > 0.0 {
+            // restarts animatiom from 0
+            shapeLayer.speed = 1.0
+            shapeLayer.timeOffset = 0.0
+        }
         
         basicAnimation.delegate = self
-        basicAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.default)
+        basicAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
         basicAnimation.fromValue = 0
         basicAnimation.toValue = 1
-        basicAnimation.duration = CFTimeInterval(preRecordMaxVideoTimeInSecs)
-        
+        basicAnimation.duration = CFTimeInterval(seconds)
         basicAnimation.fillMode = CAMediaTimingFillMode.forwards
         basicAnimation.isRemovedOnCompletion = false
         
-        timerShapeLayer.add(basicAnimation, forKey: "timerAnimation")
+        CATransaction.setCompletionBlock {
+            print("CATransaction completion called")
+        }
+        
+        shapeLayer.add(basicAnimation, forKey: "myAnimation")
+        
+        CATransaction.commit()
     }
     
-    fileprivate func removeProgressAnimation() {
-        timerShapeLayer.removeAnimation(forKey: "timerAnimation")
+    fileprivate func removeAnimation() {
+        shapeLayer.removeAnimation(forKey: "myAnimation")
     }
     
-    fileprivate func pauseAnimation() {
-        let pausedTime = timerShapeLayer.convertTime(CACurrentMediaTime(), from: nil)
-        timerShapeLayer.speed = 0.0
-        timerShapeLayer.timeOffset = pausedTime
+    fileprivate func pauseShapeLayerAnimation() {
+        
+        let pausedTime = shapeLayer.convertTime(CACurrentMediaTime(), from: nil)
+        shapeLayer.speed = 0.0
+        shapeLayer.timeOffset = pausedTime
+        
+        print("You can do something here now that the animation has stopped")
     }
     
-    fileprivate func resumeAnimation() {
-        let pausedTime = timerShapeLayer.timeOffset
-        timerShapeLayer.speed = 1.0
-        timerShapeLayer.timeOffset = 0.0
-        timerShapeLayer.beginTime = 0.0
-        let timeSincePause = timerShapeLayer.convertTime(CACurrentMediaTime(), from: nil) - pausedTime
-        timerShapeLayer.beginTime = timeSincePause
+    fileprivate func resumeShapeLayerAnimation() {
+        
+        let pausedTime = shapeLayer.timeOffset
+        shapeLayer.speed = 1.0
+        shapeLayer.timeOffset = 0.0
+        shapeLayer.beginTime = 0.0
+        let timeSincePause = shapeLayer.convertTime(CACurrentMediaTime(), from: nil) - pausedTime
+        shapeLayer.beginTime = timeSincePause
+    }
+    
+    //MARK:- Anchors
+    fileprivate func configureAnchors() {
+        
+        view.addSubview(box)
+        view.addSubview(cameraButton)
+        view.addSubview(timerLabel)
+        
+        box.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 3).isActive = true
+        box.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 3).isActive = true
+        box.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -3).isActive = true
+        box.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -3).isActive = true
+        
+        cameraButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 0).isActive = true
+        cameraButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        cameraButton.widthAnchor.constraint(equalToConstant: 75).isActive = true
+        cameraButton.heightAnchor.constraint(equalToConstant: 75).isActive = true
+        
+        timerLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+        timerLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        timerLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
     }
 }
 
@@ -180,6 +248,10 @@ class ViewController: UIViewController {
 extension ViewController: CAAnimationDelegate  {
     
     func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        print("***** animation done *****")
+        //isBasicAnimationAnimating = false
+        //resetTimerSecsAndInvalidateVideoTimer()
+        //pauseAnimation()
     }
 }
 
@@ -197,9 +269,37 @@ extension ViewController {
     }
     
     @objc fileprivate func timerIsRunning() {
-        // https://teamtreehouse.com/community/swift-countdown-timer-of-60-seconds
         
         updateTimerLabel()
+        
+        if !isBasicAnimationAnimating {
+            isBasicAnimationAnimating = true
+            
+            addProgressAnimation()
+        }
+        
+        // *** SECONDS && MILLISECONDS ONLY ***
+        milliseconds -= 1
+
+        if milliseconds < 0 {
+
+            milliseconds = 9
+
+            if seconds != 0 {
+                seconds -= 1
+
+            } else {
+
+                invalidateVideoTimer()
+
+                print("timer done")
+            }
+        }
+
+        if milliseconds == 0 {
+
+            milliseconds = 0
+        }
         
         // *** MINUTES && SECONDS && MILLISECONDS ***
         /*
@@ -240,52 +340,6 @@ extension ViewController {
             resetTimerSecsAndInvalidateVideoTimer()
         }
         */
-        
-        // *** SECONDS ONLY ***
-        
-         if !isBasicAnimationAnimating {
-         isBasicAnimationAnimating = true
-         
-         //addProgressAnimation()
-         }
-        
-        /*
-        // *** SECONDS ONLY ***
-        if seconds != 0 {
-            seconds -= 1
-            
-        } else {
-            
-            invalidateVideoTimer()
-        }
-        */
-        
-        
-        // *** SECONDS && MILLISECONDS ONLY ***
-        milliseconds -= 1
-        
-        if milliseconds < 0 {
-            
-            milliseconds = 9
-            
-            if seconds != 0 {
-                seconds -= 1
-                
-            } else {
-                
-                invalidateVideoTimer()
-                
-                resetTimerSecsAndInvalidateVideoTimer()
-            }
-            
-            //milliseconds = 9
-        }
-        
-        if milliseconds == 0 {
-            
-            milliseconds = 0
-        }
-        
     }
     
     fileprivate func updateTimerLabel() {
@@ -294,13 +348,13 @@ extension ViewController {
         let secondsStr = seconds > 9 ? "\(seconds)" : "0\(seconds)"
         
         timerLabel.text = "\(secondsStr).\(millisecStr)"
+        //timerLabel.text = "\(secondsStr)"
         
         //let minutesStr = minutes > 9 ? "\(minutes)" : "0\(minutes)"
         //timerLabel.text = "\(minutesStr):\(secondsStr).\(millisecStr)"
     }
     
     fileprivate func resetTimerSecsAndInvalidateVideoTimer() {
-        invalidateVideoTimer()
         
         // *** MINUTES && SECONDS && MILLISECONDS ***
         /*
@@ -318,9 +372,17 @@ extension ViewController {
     }
     
     fileprivate func invalidateVideoTimer() {
+        
+        if isBasicAnimationAnimating {
+            isBasicAnimationAnimating = false
+            
+            pauseShapeLayerAnimation()
+            
+            // this is just to immediatley remove the animation, don't uncomment this out
+            //removeAnimation()
+        }
+        
         videoTimer?.invalidate()
-        isBasicAnimationAnimating = false
-        pauseAnimation()
     }
 }
 
@@ -330,30 +392,35 @@ extension ViewController {
     fileprivate func configureGestures() {
         
         let takePhotoaTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(takePhotoGesture))
+        //takePhotoaTapRecognizer.numberOfTouchesRequired = 1
         cameraButton.addGestureRecognizer(takePhotoaTapRecognizer)
         
         let recordVideoLongPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(recordVideoGesture))
-        //recordVideoLongPressGestureRecognizer.minimumPressDuration = 0.33
+        //recordVideoLongPressGestureRecognizer.minimumPressDuration = 0.1
         cameraButton.addGestureRecognizer(recordVideoLongPressGestureRecognizer)
     }
     
     @objc private func takePhotoGesture(recognizer: UITapGestureRecognizer) {
         print("tap")
-        //removeProgressAnimation()
-        startTimer()
+        //startTimer()
+        //addProgressAnimation()
     }
     
     @objc private func recordVideoGesture(recognizer: UILongPressGestureRecognizer) {
         
         switch recognizer.state {
         case .began:
-            //startTimer()
-            break
-        case .ended, .cancelled, .failed:
-            //invalidateVideoTimer()
-            break
+            resetTimerSecsAndInvalidateVideoTimer()
+            startTimer()
+            print("long gesture began")
+        case .ended, .cancelled:
+            //pauseAnimation()
+            invalidateVideoTimer()
+            print("long gesture ended or cancelled")
+        case .failed:
+            print("long gesture failed")
         default:
-            //invalidateVideoTimer()
+            //print("long gesture default")
             break
         }
     }
